3b98a843567c91502cb97ff1c146bddea58186b4
karuego <66974423+karuego@users.noreply.github.com>
2025-10-23 11:46:11 +0800

    Menambahkan utils/hari.py

---
+++ src/myapp/utils/hari.py
@@ -0,0 +1,32 @@
+NAMA_HARI: list[str] = [
+    "_",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jumat",
+    "Sabtu",
+    "Minggu",
+]
+
+
+class Hari:
+    @staticmethod
+    def getNama(id: int) -> str | None:
+        if 0 < id < len(NAMA_HARI):
+            return NAMA_HARI[id]
+        return None
+
+    @staticmethod
+    def getId(nama: str) -> int:
+        target: str = nama.lower()
+
+        for i, s in enumerate(NAMA_HARI):
+            if s.lower() == target:
+                return i
+
+        return -1
+
+    @staticmethod
+    def getAll() -> list[str]:
+        return NAMA_HARI[1:]
